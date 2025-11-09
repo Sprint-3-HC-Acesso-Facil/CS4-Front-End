ccd48fed3608a30d3a95529bc7750929f606c0a4
FelypeMaschio <felypefmaschio@gmail.com>
2025-11-09 04:45:58 -0300

    Arrumando problema no Contatos 2

--- src/paginas/Contato.tsx
+++ src/paginas/Contato.tsx
@@ -9,7 +9,7 @@ export function Contato() {
   const [enviandoFormulario, setEnviandoFormulario] = useState(false)
   const [mensagemSucesso, setMensagemSucesso] = useState('')
 
-  // useEstadoLocal para salvar rascunho do formulári
+  // useEstadoLocal para salvar rascunho do formulário
   const [rascunhoFormulario, setRascunhoFormulario] = useEstadoLocal<Partial<DadosFormularioContato>>('rascunho-contato', {})
 
   // React Hook Form
